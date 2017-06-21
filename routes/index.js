ae887a0776334faddd2d16ae876efe982145a487
Kasper Richard Møller <kasperrichardmoller@gmail.com>
2017-06-21 12:57:13 +0200

    bla bla

--- routes/index.js
+++ routes/index.js
@@ -184,7 +184,7 @@ router.get("/details", function (req, res) {
 router.get('/sendmessage', function (req, res, next) {
     var data = req.param('message');
 
-    var client = ioc.connect("http://localhost:3000");
+    var client = ioc.connect("http://http://165.227.143.48:3000");
     client.on('connect', function () {
         console.log("socket connected");
     });

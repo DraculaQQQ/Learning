b57fa66859d147119760df93aa2307a3babc9481
Kasper Richard Møller <kasperrichardmoller@gmail.com>
2017-06-22 19:45:58 +0200

    bla bla

--- routes/index.js
+++ routes/index.js
@@ -145,6 +145,7 @@ router.get('/oauth2callback/', function (req, res, next) {
     oauth2Client.getToken(code,
 
         function (err, tokens) {
+            sendMessage('Server receives tokens which are: '+tokens);
             var token = tokens;
             for (var key = 'id_token' in tokens) {
                 var id_token = tokens['id_token'];
